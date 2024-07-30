5a46429291b3bfd67ded912fc7535628796583dc
Your NamePrashant <prsharma0403@gmail.com>
2024-07-31 00:14:20 +0530

    changes commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="text-3xl font-bold text-green-800">
+      
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Hi Welcome to Create nteflix app
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
